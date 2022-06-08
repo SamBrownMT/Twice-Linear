970b1bc1b9a496e67ffc606e8713b26fac6d6901
Sam Brown <sam.brown@Sam-Brown-MBP.home>
2022-06-08 13:16:10 +0100

    revised loop for u[n]

--- lib/twice_linear.rb
+++ lib/twice_linear.rb
@@ -5,12 +5,14 @@ class TwiceLinear
 
 		i = 0
 		n.times do 
-			u.push(u[i] * 2 + 1)
-			u.push(u[i] * 3 + 1)
+			y = u[i] * 2 + 1 
+			u.push(y) unless u.include?(y)
+			z = u[i] * 3 + 1
+			u.push(z) unless u.include?(z)
 			i += 1
 		end
 
-		return u.sort.uniq[n]
+		return u.sort[n]
 	end
 
 end
---
+++ test_script.rb
@@ -0,0 +1,3 @@
+require_relative "lib/twice_linear"
+
+puts TwiceLinear.new.execute(10000000) 
