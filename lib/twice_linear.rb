ebec78241708791f9cbcf4e4b2be3857aa0e629c
Sam Brown <sam.brown@Sam-Brown-MBP.home>
2022-06-09 11:55:38 +0100

    minor refactor on loop

--- lib/twice_linear.rb
+++ lib/twice_linear.rb
@@ -4,11 +4,9 @@ class TwiceLinear
 		u = [1]
 
 		i = 0
-		n.times do 
-			y = u[i] * 2 + 1 
-			u.push(y) unless u.include?(y)
-			z = u[i] * 3 + 1
-			u.push(z) unless u.include?(z)
+		n.times do  
+			u |= [u[i] * 2 + 1 ]
+			u |= [u[i] * 3 + 1]
 			i += 1
 		end
 
